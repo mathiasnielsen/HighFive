5f7dcac18865249e7621b974ab5cafbd636cb8db
Mathias Kirkegaard Nielsen <matn@bluefragments.com>
2016-04-16 15:29:54 +0200

    Before publishing

---
+++ Source/HighFive.Server.Api/Controllers/HighFiveController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace HighFive.Server.Api.Controllers
+{
+    [RoutePrefix("api")]
+    public class HighFiveController : ApiController
+    {
+        [Route("highfive/{name}")]
+        [HttpPost]
+        public string HighFive(string name)
+        {
+            return $"You slapped! Thanks {name} - {DateTime.Now.ToString("yyyy MMM dd HH:mm")}";
+        }
+    }
+}
---
+++ Source/HighFive.Server.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HighFive.Server.Api.WebApiApplication" Language="C#" %>
